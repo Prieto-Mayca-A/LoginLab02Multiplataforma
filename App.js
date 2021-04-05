74e8bdc9096106d69efab302eed75fe38e0cdb0e
Prieto-Mayca-A <68166806+Prieto-Mayca-A@users.noreply.github.com>
2021-04-04 21:08:30 -0500

    first commit

--- App.js
+++ App.js
@@ -1,192 +1,95 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React,{Component} from 'react';
+import {StyleSheet, TouchableOpacity, Text, View, Image, TextInput} from 'react-native';
+import Message from './App/components/message/Message';
+import Body from './App/components/body/Body';
+import Lista from './App/components/list/myList';
+import Edad from './App/components/validator/ageValidator';
 
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-  Alert,
-  Image
-} from 'react-native';
-
-export default class LoginView extends Component {
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Image source={require("./img/images.png")} />
-        <Text></Text> 
-        <View style={styles.inputContainer}>  
-        <Image style={[styles.icon, styles.inputIcon]} source={{uri: 'https://definicion.de/wp-content/uploads/2019/06/perfildeusuario.jpg'}}/>
-          <TextInput style={styles.inputs}
-              placeholder="Usuariol"
-              underlineColorAndroid='transparent'/>
-        </View>
-
-        <View style={styles.inputContainer}>
-          <Image style={[styles.icon, styles.inputIcon]} source={{uri: 'https://cdn.icon-icons.com/icons2/2387/PNG/512/public_padlock_unlock_person_people_icon_144648.png'}}/>
-          <TextInput style={styles.inputs}
-              placeholder="Contraseña"
-
-              underlineColorAndroid='transparent'/>
-        </View>
-     
-        <TouchableOpacity style={styles.restoreButtonContainer}>
-            <Text>Se olvido su contraseña?</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={[styles.buttonContainer, styles.loginButton]}>
-          <Text style={styles.loginText}>Login</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#DC2323',
+const provincias = [
+  {
+    id:1,
+    name: 'Arequipa',
   },
-  inputContainer: {
-      borderBottomColor: '#F5FCFF',
-      backgroundColor: '#FFFFFF',
-      borderRadius:30,
-      borderBottomWidth: 1,
-      width:250,
-      height:45,
-      marginBottom:15,
-      flexDirection: 'row',
-      alignItems:'center'
+  {
+    id: 2,
+    name: 'Lima',
   },
-  inputs:{
-      height:45,
-      marginLeft:16,
-      borderBottomColor: '#FFFFFF',
-      flex:1,
+  {
+    id: 3,
+    name: 'Piura',
   },
-  icon:{
-    width:30,
-    height:30,
-  },
-  inputIcon:{
-    marginLeft:15,
-    justifyContent: 'center'
-  },
-  buttonContainer: {
-    height:45,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom:20,
-    width:250,
-    borderRadius:30,
-  },
-  loginButton: {
-    backgroundColor: '#1C1C1C',
-  },
-  loginText: {
-    color: 'white',
-  },
-  socialIcon:{
-    color: "#FFFFFF",
-    marginRight:5
-  }
-});
+];
 
- /**
-import React,  {Component} from 'react';
-import {StyleSheet, 
-  TouchableOpacity, 
-  Text, 
-  View,
-  TextInput, 
-  Image
-} from 'react-native';
-
-export default class App extends Component {
-  constructor(props) {
+export default class App extends Component{
+  constructor(props){
     super(props);
+
     this.state = {
       textValue: '',
       count: 0,
     };
   }
 
-  changeTextInput = text => {
+  changeTextInput = text =>{
+    console.log(text)
     this.setState({textValue: text});
   };
-
+  
   onPress = () => {
     this.setState({
       count: this.state.count + 1,
     });
-  };
+};
 
-  render() {
-    return (
+  render(){
+    return(
       <View style={styles.container}>
         <View style={styles.text}>
-          <Text> Hola amigos</Text>
+        <Text>Tarea del laboratorio03 </Text>
         </View>
-
-        <Image source={require("./img/logo.png")} />
-
-        <TextInput
-          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-          onChangeText={text => this.changeTextInput(text)}
-          value={this.state.textValue} 
-        />
-
-        <TouchableOpacity style={styles.button} onPress={this.onPress}>
-          <Text> Touch Here </Text>
-        </TouchableOpacity>
-        <View style={[styles.countContainer]} >
-          <Text style={[styles.countText]}>
-            {this.state.count}
-          </Text>
-        </View> 
-      </View>
-    );    
+        <Text>Ingrese su edad por favor : </Text>
+        <Edad/>
+        <Text>Flat List react native</Text>
+        <Lista/>
+        <Text></Text>
+        <View style={styles.text}>
+        </View>
+        </View>
+    );
   }
 }
 
-
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
     justifyContent: 'center',
     paddingHorizontal: 10,
   },
-  text: {
+  text:{
     alignItems: 'center',
     padding: 10,
-  },
-
-  button: {
-    top: 10,
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 10,
-  },
-
-  countContainer:{
-    alignItems: 'center',
-    padding: 10,
-  },
-
-  countText: {
-    color: '#FF00FF'
   },
 });
+/*
+<Text>Lo que se hizo en el lab03</Text>
+        </View>
+        <View style={styles.text}>
+          <Text>Ingrese su edad: </Text>
+        </View>
+          <TextInput 
+          style={{height:40, borderColor: 'red', borderWidth: 1}}
+          onChangeText={text => this.changeTextInput(text)}
+          value={this.state.textValue}
+          />
+          <Body textBody={'Texto en Body'} onBodyPress={this.onPress}/>
+
+          <View style={[styles.countContainer]}>
+            <Text style={[styles.countText]}>{this.state.count}</Text>
+          </View>
+
+          {provincias.map(item => (
+            <View>
+              <Text>{item.name}</Text>
+            </View>
+          ))}
 */
